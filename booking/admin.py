d83c6b9bd19ac5aab1a8eea54df8e524aca65f79
Gowthamkannappan <gowtham@techis.io>
2023-08-02 22:55:21 +0530

    Working on models for Phase I

--- booking/admin.py
+++ booking/admin.py
@@ -1,21 +1,3 @@
 from django.contrib import admin
-from .models import Vehicle, Complaint, Service
 
-class VehicleAdmin(admin.ModelAdmin):
-    list_display = ['name', 'type', 'application']
-    list_filter = ['type', 'application']
-    search_fields = ['name', 'type', 'application']
-
-class ComplaintAdmin(admin.ModelAdmin):
-    list_display = ['complaint_type', 'complaint_description']
-    list_filter = ['complaint_type']
-    search_fields = ['complaint_type', 'complaint_description']
-
-class ServiceAdmin(admin.ModelAdmin):
-    list_display = ['name', 'vehicle', 'component' ,'service_booking_date','mobile', 'service_type']
-    list_filter = ['service_type']
-    search_fields = ['name', 'vehicle__name', 'component','service_booking_date','mobile']
-
-admin.site.register(Vehicle, VehicleAdmin)
-admin.site.register(Complaint, ComplaintAdmin)
-admin.site.register(Service, ServiceAdmin)
+# Register your models here.
--- booking/constants.py
+++ booking/constants.py
@@ -1,50 +1,9 @@
-APPLICATION_CHOICES = (
-    ('Agriculture', 'Agriculture'),
-    ('Commercial', 'Commercial'),
-    ("Domestic", 'Domestic'),
-)
+from django.db import models
 
-COMPLAINT_CHOICES = (
-    ('GENERAL_CHECKUP', 'General Checkup'),
-    ('SPECIFIC_COMPLAINT', 'Specific Complaint'),
-)
+# Function to validate the postal code 
 
-SERVICE_CHOICES=(
-    ('Single Cyl- Pump', 'Single Cyl- Pump'),
-    ('VE - Pump', 'VE - Pump'),
-    ('Inline 6cyl - Pump', 'Inline 6cyl - Pump'),
-    ('Inline 4cyl - Pump', 'Inline 4cyl - Pump'),
-    ('Inline 3cyl - Pump', 'Inline 3cyl - Pump'),
-    ('VE-EDC - Pump', 'VE-EDC - Pump'),
-    ('CRDI - Pump', 'CRDI - Pump'),
-    ('China - Pump', 'China - Pump'),
-    ('Injectors only', 'Injectors Only'),
-    ('CR-Injectors Only', 'CR-Injectors Only'),
+def validate_postal_code(value):
+    if not value.isdigit() or len(value) != 6:
+        raise models.ValidationError("Postal code must be a 6-digit number.")
 
-)
-
-VEHICLE_CHOICES = (
-    ("Oil Engines", 'Oil Engine'),
-    ('Auto', 'Auto'),
-    ('Bus', 'Bus'),
-    ('Car', 'Car'),
-    ('JCB Loader', 'JCB Loader'),
-    ('Generator', 'Generator'),
-    ('Marine Engines', 'Marine Engines'),
-    ('Tractor', 'Tractor'),
-    ('Tipper', 'Tipper'),
-    ('6 wheel - Truck', '6 wheel - Truck'),
-    ('10 wheel - Truck', '10 wheel - Truck'),
-    ('12 wheel - Truck', '12 wheel - Truck'),
-    ('Tanker Lorry', 'Tanker Lorry'),
-    ('Van', 'Van'),
-    ('Others', 'Others')
-    # Add other choices here
-)
-
-QUICK_SERVICE = 'Quick Service'
-REGULAR_SERVICE = 'Regular Service'
-SERVICE_TYPE_CHOICES = (
-    (QUICK_SERVICE, 'Quick Service'),
-    (REGULAR_SERVICE, 'Regular Service'),
-)
+COMPONENT_CHOICES=[]
